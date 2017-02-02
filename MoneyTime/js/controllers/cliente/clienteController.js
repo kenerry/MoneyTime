af70a1a9eeeb6e5e533b7e44eae1bfbfeddd84a6
Kenerry <kenerry13@gmail.com>
2017-02-02 00:53:36 -0200

    Personalizando front

--- MoneyTime/js/controllers/cliente/clienteController.js
+++ MoneyTime/js/controllers/cliente/clienteController.js
@@ -19,9 +19,9 @@
                  { nome: "Kenerry Serain", email: "kenerry13@gmail.com", celular: "(11) 96057-4281", dataNascimento: "16/11/1995"},
                  { nome: "Kenerry Serain", email: "kenerry13@gmail.com", celular: "(11) 96057-4281", dataNascimento: "16/11/1995"},
             ];
-
+$scope.limitOptions = [5, 10, 15];
             };
-
+  
             angular.module("MoneyTime").controller("clienteController", clienteController);     
 }());
 
